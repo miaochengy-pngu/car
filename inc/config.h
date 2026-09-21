27ec7e243c06f9affe9ba76199eb0d2d64e17423
Chengyu Miao <815407717@qq.com>
2026-09-21 20:50:55 +0800

    Add modular C51 firmware drivers

---
+++ inc/config.h
@@ -0,0 +1,23 @@
+#ifndef __CAR_CONFIG_H__
+#define __CAR_CONFIG_H__
+
+/*
+ * A11 C51 line-following car configuration.
+ * Verified against the kit manual and the user's PCB silkscreen.
+ */
+
+/* TCRT5000 module: white background = 0, black line = 1 */
+#define TRACK_BLACK_LEVEL 1
+
+/* Steering mode */
+#define TURN_STYLE_ARC   0
+#define TURN_STYLE_SPIN  1
+#define TURN_STYLE       TURN_STYLE_ARC
+
+/* Stop when both sensors see black (useful as a finish line). */
+#define STOP_ON_BOTH_BLACK 1
+
+/* Main control-loop period. */
+#define CONTROL_PERIOD_MS 2
+
+#endif
