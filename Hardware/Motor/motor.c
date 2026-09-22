323cb5598957d94c36f9069a3d69f62bb63a8efe
Chengyu Miao <815407717@qq.com>
2026-09-22 19:52:47 +0800

    Upgrade to competition PWM and PID line control

--- Hardware/Motor/motor.c
+++ Hardware/Motor/motor.c
@@ -1,112 +1,109 @@
 #include <REG52.H>
-#include "config.h"
+#include "pwm.h"
 #include "motor.h"
 
 /*
  * A11 main-board motor-control header:
- *   ENB -> P1.0
+ *   ENB -> P1.0   (software PWM, Hardware/PWM)
  *   IN4 -> P1.1
  *   IN3 -> P1.2
  *   IN2 -> P1.3
  *   IN1 -> P1.4
- *   ENA -> P1.5
+ *   ENA -> P1.5   (software PWM, Hardware/PWM)
  */
-sbit MOTOR_R_EN  = P1^0;   /* ENB */
-sbit MOTOR_R_IN4 = P1^1;   /* IN4 */
-sbit MOTOR_R_IN3 = P1^2;   /* IN3 */
-sbit MOTOR_L_IN2 = P1^3;   /* IN2 */
-sbit MOTOR_L_IN1 = P1^4;   /* IN1 */
-sbit MOTOR_L_EN  = P1^5;   /* ENA */
+sbit MOTOR_R_IN4 = P1^1;
+sbit MOTOR_R_IN3 = P1^2;
+sbit MOTOR_L_IN2 = P1^3;
+sbit MOTOR_L_IN1 = P1^4;
 
-void motor_enable(void)
+static int clamp_command(int command)
 {
-    MOTOR_L_EN = 1;
-    MOTOR_R_EN = 1;
+    if (command > 100)
+    {
+        return 100;
+    }
+
+    if (command < -100)
+    {
+        return -100;
+    }
+
+    return command;
 }
 
-void motor_disable(void)
+static unsigned char abs_command(int command)
 {
-    MOTOR_L_EN = 0;
-    MOTOR_R_EN = 0;
+    if (command < 0)
+    {
+        command = -command;
+    }
+
+    return (unsigned char)command;
 }
 
-/*
- * Polarity follows the A11 manual truth table.
- * Left : IN1=1, IN2=0 -> forward.
- * Right: IN3=0, IN4=1 -> forward.
- */
-void left_motor_forward(void)
+static void set_left_direction(int command)
 {
-    MOTOR_L_IN1 = 1;
-    MOTOR_L_IN2 = 0;
+    if (command > 0)
+    {
+        /* A11 manual: IN1=1, IN2=0 -> left forward */
+        MOTOR_L_IN1 = 1;
+        MOTOR_L_IN2 = 0;
+    }
+    else if (command < 0)
+    {
+        MOTOR_L_IN1 = 0;
+        MOTOR_L_IN2 = 1;
+    }
+    else
+    {
+        MOTOR_L_IN1 = 0;
+        MOTOR_L_IN2 = 0;
+    }
 }
 
-void left_motor_reverse(void)
+static void set_right_direction(int command)
+{
+    if (command > 0)
+    {
+        /* A11 manual: IN3=0, IN4=1 -> right forward */
+        MOTOR_R_IN3 = 0;
+        MOTOR_R_IN4 = 1;
+    }
+    else if (command < 0)
+    {
+        MOTOR_R_IN3 = 1;
+        MOTOR_R_IN4 = 0;
+    }
+    else
+    {
+        MOTOR_R_IN3 = 0;
+        MOTOR_R_IN4 = 0;
+    }
+}
+
+void motor_init(void)
 {
     MOTOR_L_IN1 = 0;
-    MOTOR_L_IN2 = 1;
-}
-
-void left_motor_stop(void)
-{
-    MOTOR_L_IN1 = 0;
     MOTOR_L_IN2 = 0;
-}
-
-void right_motor_forward(void)
-{
-    MOTOR_R_IN3 = 0;
-    MOTOR_R_IN4 = 1;
-}
-
-void right_motor_reverse(void)
-{
-    MOTOR_R_IN3 = 1;
-    MOTOR_R_IN4 = 0;
-}
-
-void right_motor_stop(void)
-{
     MOTOR_R_IN3 = 0;
     MOTOR_R_IN4 = 0;
+
+    pwm_set_both(0, 0);
 }
 
-void car_stop(void)
+void motor_set(int left_command, int right_command)
 {
-    left_motor_stop();
-    right_motor_stop();
+    left_command = clamp_command(left_command);
+    right_command = clamp_command(right_command);
+
+    set_left_direction(left_command);
+    set_right_direction(right_command);
+
+    pwm_set_left(abs_command(left_command));
+    pwm_set_right(abs_command(right_command));
 }
 
-void car_forward(void)
+void motor_stop(void)
 {
-    left_motor_forward();
-    right_motor_forward();
-}
-
-void car_backward(void)
-{
-    left_motor_reverse();
-    right_motor_reverse();
-}
-
-void car_turn_left(void)
-{
-#if TURN_STYLE == TURN_STYLE_SPIN
-    left_motor_reverse();
-    right_motor_forward();
-#else
-    left_motor_stop();
-    right_motor_forward();
-#endif
-}
-
-void car_turn_right(void)
-{
-#if TURN_STYLE == TURN_STYLE_SPIN
-    left_motor_forward();
-    right_motor_reverse();
-#else
-    left_motor_forward();
-    right_motor_stop();
-#endif
+    motor_set(0, 0);
 }
