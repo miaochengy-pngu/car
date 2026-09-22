f307814ec3141911cfb3db9a93abe79ebf2f18d5
Chengyu Miao <815407717@qq.com>
2026-09-22 19:52:50 +0800

    Upgrade to competition PWM and PID line control

--- Hardware/Tracking/tracking.h
+++ Hardware/Tracking/tracking.h
@@ -1,7 +1,15 @@
 #ifndef __TRACKING_H__
 #define __TRACKING_H__
 
+#define TRACK_PATTERN_BOTH_WHITE  0
+#define TRACK_PATTERN_RIGHT_BLACK 1
+#define TRACK_PATTERN_LEFT_BLACK  2
+#define TRACK_PATTERN_BOTH_BLACK  3
+
 void tracking_init(void);
-void tracking_control(void);
+void tracking_calibrate_center(void);
+
+unsigned char tracking_read_pattern(void);
+unsigned char tracking_get_center_pattern(void);
 
 #endif
