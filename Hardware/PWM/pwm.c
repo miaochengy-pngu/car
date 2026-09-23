26d401d4e96026af6db56de2648cf6f6c40f6e69
Chengyu Miao <815407717@qq.com>
2026-09-23 15:16:46 +0800

    Add filtered hysteresis turn state machine

--- Hardware/PWM/pwm.c
+++ Hardware/PWM/pwm.c
@@ -3,7 +3,8 @@
 
 /*
  * 参考公开的 STC89C52RC 循迹小车例程写法：
- * Timer0 中断只负责软件 PWM；主循环只负责读循迹传感器并改占空比。
+ * Timer0 中断只负责软件 PWM 和 1 ms 系统节拍；
+ * 主循环只负责读循迹传感器并改占空比。
  *
  * A11 主控板：
  *   ENA -> P1.5 -> 左电机 PWM
@@ -16,13 +17,10 @@ sbit PWM_RIGHT_EN = P1^0;
  * 11.0592 MHz、传统 12T：
  * 约 1 ms Timer0 中断。
  *
- * 公开 STC89C52RC 小车例程常用“1 ms 中断 + PWM 计数器”的结构。
- * 这里使用 20 级 PWM：
+ * 20 级 PWM：
  *   20 x 1 ms = 20 ms
  *   PWM 频率约 50 Hz
  *   分辨率 5%
- *
- * PWM 完全由中断产生，不在 while(1) 里延时产生。
  */
 #define TIMER0_RELOAD_H  0xFC
 #define TIMER0_RELOAD_L  0x66
@@ -31,6 +29,7 @@ sbit PWM_RIGHT_EN = P1^0;
 static volatile unsigned char g_left_duty_steps = 0;
 static volatile unsigned char g_right_duty_steps = 0;
 static volatile unsigned char g_pwm_counter = 0;
+static volatile unsigned char g_ms_tick = 0;
 
 static unsigned char percent_to_steps(unsigned char percent)
 {
@@ -59,6 +58,7 @@ void pwm_init(void)
     g_left_duty_steps = 0;
     g_right_duty_steps = 0;
     g_pwm_counter = 0;
+    g_ms_tick = 0;
 
     TMOD &= 0xF0;
     TMOD |= 0x01;       /* Timer0 mode 1 */
@@ -98,10 +98,17 @@ void pwm_set_both(unsigned char left_duty, unsigned char right_duty)
     pwm_set_right(right_duty);
 }
 
+unsigned char pwm_millis8(void)
+{
+    return g_ms_tick;
+}
+
 void Timer0_ISR(void) interrupt 1 using 1
 {
     TH0 = TIMER0_RELOAD_H;
     TL0 = TIMER0_RELOAD_L;
+
+    g_ms_tick++;
 
     g_pwm_counter++;
 
