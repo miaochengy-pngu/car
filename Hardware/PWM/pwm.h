41e1b08a8083598bfef2a844890474a48e2c711f
Chengyu Miao <815407717@qq.com>
2026-09-23 10:58:21 +0800

    Match proven STC89C52 Timer0 PWM polling pattern

--- Hardware/PWM/pwm.h
+++ Hardware/PWM/pwm.h
@@ -2,16 +2,12 @@
 #define __PWM_H__
 
 /*
- * Software PWM on L298N ENA/ENB.
- * Duty arguments use 0..100 percent.
+ * 软件 PWM 由 Timer0 中断持续输出。
+ * 主循环只调用这些函数修改目标占空比。
  */
-
 void pwm_init(void);
 void pwm_set_left(unsigned char duty);
 void pwm_set_right(unsigned char duty);
 void pwm_set_both(unsigned char left_duty, unsigned char right_duty);
-
-unsigned char pwm_control_tick_ready(void);
-void pwm_clear_control_tick(void);
 
 #endif
