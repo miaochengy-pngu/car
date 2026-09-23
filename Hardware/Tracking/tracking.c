8748c60be7ac8e42fd6507a37ad370a62c9ff1aa
Chengyu Miao <815407717@qq.com>
2026-09-23 16:11:19 +0800

    Replace controller with direct STC89C52 open-source line-following logic

--- Hardware/Tracking/tracking.c
+++ Hardware/Tracking/tracking.c
@@ -1,38 +1,37 @@
 #include <REG52.H>
-#include "config.h"
 #include "tracking.h"
 
 /*
- * VER:2.3 主控板红外循迹接口：
+ * 你的 A11 板：
  *   OTL -> P3.5
  *   OTR -> P3.4
+ *
+ * 实车已确认：
+ *   灯亮 = 0
+ *   灯灭 = 1
+ *
+ * 因此直接返回两位原始数字量：
+ *   00 两灯亮
+ *   10 左灯灭
+ *   01 右灯灭
+ *   11 两灯灭
  */
 sbit TRACK_LEFT  = P3^5;
 sbit TRACK_RIGHT = P3^4;
 
-static unsigned char is_black(unsigned char level)
-{
-#if TRACK_BLACK_LEVEL == 1
-    return level ? 1 : 0;
-#else
-    return level ? 0 : 1;
-#endif
-}
-
 void tracking_init(void)
 {
-    /* 8051 准双向口作输入时先写 1 */
     TRACK_LEFT = 1;
     TRACK_RIGHT = 1;
 }
 
 unsigned char tracking_read_pattern(void)
 {
-    unsigned char left_black;
-    unsigned char right_black;
+    unsigned char left;
+    unsigned char right;
 
-    left_black = is_black(TRACK_LEFT);
-    right_black = is_black(TRACK_RIGHT);
+    left = TRACK_LEFT ? 1 : 0;
+    right = TRACK_RIGHT ? 1 : 0;
 
-    return (unsigned char)((left_black << 1) | right_black);
+    return (unsigned char)((left << 1) | right);
 }
