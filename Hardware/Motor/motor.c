c06f6d579f02ef27eba0ee2a95bd8ea63d09f23c
Chengyu Miao <815407717@qq.com>
2026-09-23 16:45:11 +0800

    Bypass PWM and drive motor enables directly for hardware isolation

--- Hardware/Motor/motor.c
+++ Hardware/Motor/motor.c
@@ -1,81 +1,66 @@
 #include <REG52.H>
-#include "pwm.h"
 #include "motor.h"
 
 /*
- * A11 main-board motor-control header:
- *   ENB -> P1.0   (software PWM, Hardware/PWM)
+ * A11 主控板电机接口：
+ *   ENB -> P1.0
  *   IN4 -> P1.1
  *   IN3 -> P1.2
  *   IN2 -> P1.3
  *   IN1 -> P1.4
- *   ENA -> P1.5   (software PWM, Hardware/PWM)
+ *   ENA -> P1.5
+ *
+ * 当前为了排除软件 PWM / Timer0 问题，
+ * ENA / ENB 直接高电平全速使能。
  */
+sbit MOTOR_R_EN  = P1^0;
 sbit MOTOR_R_IN4 = P1^1;
 sbit MOTOR_R_IN3 = P1^2;
 sbit MOTOR_L_IN2 = P1^3;
 sbit MOTOR_L_IN1 = P1^4;
+sbit MOTOR_L_EN  = P1^5;
 
-static int clamp_command(int command)
-{
-    if (command > 100)
-    {
-        return 100;
-    }
-
-    if (command < -100)
-    {
-        return -100;
-    }
-
-    return command;
-}
-
-static unsigned char abs_command(int command)
-{
-    if (command < 0)
-    {
-        command = -command;
-    }
-
-    return (unsigned char)command;
-}
-
-static void set_left_direction(int command)
+static void set_left(int command)
 {
     if (command > 0)
     {
-        /* A11 manual: IN1=1, IN2=0 -> left forward */
+        /* IN1=1, IN2=0 -> 左轮正转 */
         MOTOR_L_IN1 = 1;
         MOTOR_L_IN2 = 0;
+        MOTOR_L_EN = 1;
     }
     else if (command < 0)
     {
         MOTOR_L_IN1 = 0;
         MOTOR_L_IN2 = 1;
+        MOTOR_L_EN = 1;
     }
     else
     {
+        MOTOR_L_EN = 0;
         MOTOR_L_IN1 = 0;
         MOTOR_L_IN2 = 0;
     }
 }
 
-static void set_right_direction(int command)
+static void set_right(int command)
 {
     if (command > 0)
     {
-        /* A11 manual: IN3=0, IN4=1 -> right forward */
+        /* IN3=0, IN4=1 -> 右轮正转 */
         MOTOR_R_IN3 = 0;
         MOTOR_R_IN4 = 1;
+        MOTOR_R_EN = 1;
     }
     else if (command < 0)
     {
         MOTOR_R_IN3 = 1;
         MOTOR_R_IN4 = 0;
+        MOTOR_R_EN = 1;
     }
     else
     {
+        MOTOR_R_EN = 0;
         MOTOR_R_IN3 = 0;
         MOTOR_R_IN4 = 0;
     }
@@ -83,24 +68,19 @@ static void set_right_direction(int command)
 
 void motor_init(void)
 {
+    MOTOR_L_EN = 0;
+    MOTOR_R_EN = 0;
+
     MOTOR_L_IN1 = 0;
     MOTOR_L_IN2 = 0;
     MOTOR_R_IN3 = 0;
     MOTOR_R_IN4 = 0;
-
-    pwm_set_both(0, 0);
 }
 
 void motor_set(int left_command, int right_command)
 {
-    left_command = clamp_command(left_command);
-    right_command = clamp_command(right_command);
-
-    set_left_direction(left_command);
-    set_right_direction(right_command);
-
-    pwm_set_left(abs_command(left_command));
-    pwm_set_right(abs_command(right_command));
+    set_left(left_command);
+    set_right(right_command);
 }
 
 void motor_stop(void)
