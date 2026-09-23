3921bd1b2eaef79310d7aba7c8e0782ca856fc6b
Chengyu Miao <815407717@qq.com>
2026-09-23 10:58:19 +0800

    Match proven STC89C52 Timer0 PWM polling pattern

--- Hardware/PWM/pwm.c
+++ Hardware/PWM/pwm.c
@@ -1,47 +1,51 @@
 #include <REG52.H>
 #include "pwm.h"
 
-/* A11 主控板：ENA=P1.5，ENB=P1.0 */
+/*
+ * 参考公开的 STC89C52RC 循迹小车例程写法：
+ * Timer0 中断只负责软件 PWM；主循环只负责读循迹传感器并改占空比。
+ *
+ * A11 主控板：
+ *   ENA -> P1.5 -> 左电机 PWM
+ *   ENB -> P1.0 -> 右电机 PWM
+ */
 sbit PWM_LEFT_EN  = P1^5;
 sbit PWM_RIGHT_EN = P1^0;
 
 /*
- * Fosc = 11.0592 MHz，传统 12T 8051：
- * Timer0 时钟约 921.6 kHz。
+ * 11.0592 MHz、传统 12T：
+ * 约 1 ms Timer0 中断。
  *
- * 200 us 中断一次：
- *   25 个相位 -> 5 ms PWM 周期 -> 200 Hz
- *   PWM 分辨率 4%
+ * 公开 STC89C52RC 小车例程常用“1 ms 中断 + PWM 计数器”的结构。
+ * 这里使用 20 级 PWM：
+ *   20 x 1 ms = 20 ms
+ *   PWM 频率约 50 Hz
+ *   分辨率 5%
  *
- * 每 25 次中断产生一次控制节拍：
- *   控制周期 5 ms -> 200 Hz
+ * PWM 完全由中断产生，不在 while(1) 里延时产生。
  */
-#define TIMER0_RELOAD_H   0xFF
-#define TIMER0_RELOAD_L   0x48
+#define TIMER0_RELOAD_H  0xFC
+#define TIMER0_RELOAD_L  0x66
+#define PWM_STEPS         20
 
-#define PWM_PHASES        25
-#define CONTROL_DIVIDER   25
+static volatile unsigned char g_left_duty_steps = 0;
+static volatile unsigned char g_right_duty_steps = 0;
+static volatile unsigned char g_pwm_counter = 0;
 
-static volatile unsigned char g_left_steps = 0;
-static volatile unsigned char g_right_steps = 0;
-static volatile unsigned char g_pwm_phase = 0;
-static volatile unsigned char g_control_divider = 0;
-static volatile unsigned char g_control_tick = 0;
-
-static unsigned char duty_to_steps(unsigned char duty)
+static unsigned char percent_to_steps(unsigned char percent)
 {
     unsigned char steps;
 
-    if (duty >= 100)
+    if (percent >= 100)
     {
-        return PWM_PHASES;
+        return PWM_STEPS;
     }
 
-    steps = (unsigned char)((duty + 3) / 4);
+    steps = (unsigned char)((percent + 2) / 5);
 
-    if (steps > PWM_PHASES)
+    if (steps > PWM_STEPS)
     {
-        steps = PWM_PHASES;
+        steps = PWM_STEPS;
     }
 
     return steps;
@@ -52,14 +56,12 @@ void pwm_init(void)
     PWM_LEFT_EN = 0;
     PWM_RIGHT_EN = 0;
 
-    g_left_steps = 0;
-    g_right_steps = 0;
-    g_pwm_phase = 0;
-    g_control_divider = 0;
-    g_control_tick = 0;
+    g_left_duty_steps = 0;
+    g_right_duty_steps = 0;
+    g_pwm_counter = 0;
 
     TMOD &= 0xF0;
-    TMOD |= 0x01;
+    TMOD |= 0x01;       /* Timer0 mode 1 */
 
     TH0 = TIMER0_RELOAD_H;
     TL0 = TIMER0_RELOAD_L;
@@ -77,7 +79,7 @@ void pwm_set_left(unsigned char duty)
         duty = 100;
     }
 
-    g_left_steps = duty_to_steps(duty);
+    g_left_duty_steps = percent_to_steps(duty);
 }
 
 void pwm_set_right(unsigned char duty)
@@ -87,7 +89,7 @@ void pwm_set_right(unsigned char duty)
         duty = 100;
     }
 
-    g_right_steps = duty_to_steps(duty);
+    g_right_duty_steps = percent_to_steps(duty);
 }
 
 void pwm_set_both(unsigned char left_duty, unsigned char right_duty)
@@ -96,36 +98,33 @@ void pwm_set_both(unsigned char left_duty, unsigned char right_duty)
     pwm_set_right(right_duty);
 }
 
-unsigned char pwm_control_tick_ready(void)
-{
-    return g_control_tick;
-}
-
-void pwm_clear_control_tick(void)
-{
-    g_control_tick = 0;
-}
-
 void Timer0_ISR(void) interrupt 1 using 1
 {
     TH0 = TIMER0_RELOAD_H;
     TL0 = TIMER0_RELOAD_L;
 
-    g_pwm_phase++;
+    g_pwm_counter++;
 
-    if (g_pwm_phase >= PWM_PHASES)
+    if (g_pwm_counter >= PWM_STEPS)
     {
-        g_pwm_phase = 0;
+        g_pwm_counter = 0;
     }
 
-    PWM_LEFT_EN = (g_pwm_phase < g_left_steps) ? 1 : 0;
-    PWM_RIGHT_EN = (g_pwm_phase < g_right_steps) ? 1 : 0;
-
-    g_control_divider++;
-
-    if (g_control_divider >= CONTROL_DIVIDER)
+    if (g_pwm_counter < g_left_duty_steps)
     {
-        g_control_divider = 0;
-        g_control_tick = 1;
+        PWM_LEFT_EN = 1;
+    }
+    else
+    {
+        PWM_LEFT_EN = 0;
+    }
+
+    if (g_pwm_counter < g_right_duty_steps)
+    {
+        PWM_RIGHT_EN = 1;
+    }
+    else
+    {
+        PWM_RIGHT_EN = 0;
     }
 }
