3f960d97868c20c12de521f668529abb0e49fc5a
Chengyu Miao <815407717@qq.com>
2026-09-23 16:11:12 +0800

    Replace controller with direct STC89C52 open-source line-following logic

--- Hardware/PWM/pwm.c
+++ Hardware/PWM/pwm.c
@@ -2,26 +2,16 @@
 #include "pwm.h"
 
 /*
- * 参考公开的 STC89C52RC 循迹小车例程写法：
- * Timer0 中断只负责软件 PWM 和 1 ms 系统节拍；
- * 主循环只负责读循迹传感器并改占空比。
- *
  * A11 主控板：
  *   ENA -> P1.5 -> 左电机 PWM
  *   ENB -> P1.0 -> 右电机 PWM
+ *
+ * 结构直接参考公开 STC89C52RC 循迹例程：
+ * Timer0 约 1 ms 中断，计数器 0~19 产生软件 PWM。
  */
 sbit PWM_LEFT_EN  = P1^5;
 sbit PWM_RIGHT_EN = P1^0;
 
-/*
- * 11.0592 MHz、传统 12T：
- * 约 1 ms Timer0 中断。
- *
- * 20 级 PWM：
- *   20 x 1 ms = 20 ms
- *   PWM 频率约 50 Hz
- *   分辨率 5%
- */
 #define TIMER0_RELOAD_H  0xFC
 #define TIMER0_RELOAD_L  0x66
 #define PWM_STEPS         20
@@ -29,7 +19,6 @@ sbit PWM_RIGHT_EN = P1^0;
 static volatile unsigned char g_left_duty_steps = 0;
 static volatile unsigned char g_right_duty_steps = 0;
 static volatile unsigned char g_pwm_counter = 0;
-static volatile unsigned char g_ms_tick = 0;
 
 static unsigned char percent_to_steps(unsigned char percent)
 {
@@ -58,10 +47,9 @@ void pwm_init(void)
     g_left_duty_steps = 0;
     g_right_duty_steps = 0;
     g_pwm_counter = 0;
-    g_ms_tick = 0;
 
     TMOD &= 0xF0;
-    TMOD |= 0x01;       /* Timer0 mode 1 */
+    TMOD |= 0x01;
 
     TH0 = TIMER0_RELOAD_H;
     TL0 = TIMER0_RELOAD_L;
@@ -98,17 +86,10 @@ void pwm_set_both(unsigned char left_duty, unsigned char right_duty)
     pwm_set_right(right_duty);
 }
 
-unsigned char pwm_millis8(void)
-{
-    return g_ms_tick;
-}
-
 void Timer0_ISR(void) interrupt 1 using 1
 {
     TH0 = TIMER0_RELOAD_H;
     TL0 = TIMER0_RELOAD_L;
-
-    g_ms_tick++;
 
     g_pwm_counter++;
 
