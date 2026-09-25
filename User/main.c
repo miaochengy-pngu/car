b396d81646b4f241679731087391e729d07cd84c
Chengyu Miao <815407717@qq.com>
2026-09-25 17:34:18 +0800

    initialize Timer0 before line control

--- User/main.c
+++ User/main.c
@@ -2,11 +2,13 @@
 #include "motor.h"
 #include "tracking.h"
 #include "line_control.h"
+#include "timer.h"
 
 void main(void)
 {
     tracking_init();
     motor_init();
+    timer0_init();
     line_control_init();
 
     delay_ms(300);
