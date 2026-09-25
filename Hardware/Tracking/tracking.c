2276eabe0cff73fb207475607e0bfb55de4872c5
Chengyu Miao <815407717@qq.com>
2026-09-25 16:08:44 +0800

    Add outer infrared sensors

--- Hardware/Tracking/tracking.c
+++ Hardware/Tracking/tracking.c
@@ -2,27 +2,31 @@
 #include "tracking.h"
 
 /*
- * 你的 A11 板：
- *   OTL -> P3.5
- *   OTR -> P3.4
+ * 四路红外：
  *
- * 实车已确认：
- *   灯亮 = 0
- *   灯灭 = 1
+ * 内侧：普通循迹
+ *   LEFT  -> P3.5
+ *   RIGHT -> P3.4
  *
- * 因此直接返回两位原始数字量：
- *   00 两灯亮
- *   10 左灯灭
- *   01 右灯灭
- *   11 两灯灭
+ * 外侧：90度转弯触发
+ *   OUTER_LEFT  -> P3.6
+ *   OUTER_RIGHT -> P3.7
+ *
+ * 黑线 = 1
+ * 白底 = 0
  */
-sbit TRACK_LEFT  = P3^5;
+
+sbit TRACK_LEFT = P3^5;
 sbit TRACK_RIGHT = P3^4;
+sbit OUTER_LEFT = P3^6;
+sbit OUTER_RIGHT = P3^7;
 
 void tracking_init(void)
 {
     TRACK_LEFT = 1;
     TRACK_RIGHT = 1;
+    OUTER_LEFT = 1;
+    OUTER_RIGHT = 1;
 }
 
 unsigned char tracking_read_pattern(void)
@@ -34,4 +38,14 @@ unsigned char tracking_read_pattern(void)
     right = TRACK_RIGHT ? 1 : 0;
 
     return (unsigned char)((left << 1) | right);
+}
+
+unsigned char tracking_outer_left(void)
+{
+    return OUTER_LEFT ? 1 : 0;
+}
+
+unsigned char tracking_outer_right(void)
+{
+    return OUTER_RIGHT ? 1 : 0;
 }
