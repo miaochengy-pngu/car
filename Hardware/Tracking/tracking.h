8863c5537faa28b1d374506ab8728afb40e8865b
Chengyu Miao <815407717@qq.com>
2026-09-25 16:08:39 +0800

    Add outer turn sensors for junction turning

--- Hardware/Tracking/tracking.h
+++ Hardware/Tracking/tracking.h
@@ -7,6 +7,12 @@
 #define TRACK_PATTERN_BOTH_BLACK   3
 
 void tracking_init(void);
+
+/* 内侧两个传感器：普通循迹 */
 unsigned char tracking_read_pattern(void);
+
+/* 外侧两个传感器：90度转弯触发 */
+unsigned char tracking_outer_left(void);
+unsigned char tracking_outer_right(void);
 
 #endif
