aecc054459a63be366d97e16b854634f2446dbdf
Chengyu Miao <815407717@qq.com>
2026-09-26 18:21:04 +0800

    move state timing API to Timer1

--- System/timer.h
+++ System/timer.h
@@ -1,7 +1,11 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
-void timer0_init(void);
-unsigned int timer0_get_ms(void);
+/*
+ * Timer1 专门提供状态机毫秒时基。
+ * Timer0 留给 PWM，不与计时器抢占。
+ */
+void timer1_init(void);
+unsigned int timer1_get_ms(void);
 
 #endif
